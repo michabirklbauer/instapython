9f66165e6b8f40f7f6f6410370849152aa1dfbba
Micha Birklbauer <micha.birklbauer@gmail.com>
2018-07-25 23:17:45 +0200

    bugfixed class order

--- InstaPython/InstaPython/__init__.py
+++ InstaPython/InstaPython/__init__.py
@@ -15,218 +15,6 @@ import time
 import sys
 import os
 
-class InstaBot:
-
-	instagram = Instagram()
-
-	def __init__(self, path_to_config_file, channel, api_token):
-		self.path_to_config_file = path_to_config_file
-		self.channel = channel
-		self.api_token = api_token
-
-	def getUserName(self, user_id):
-		time.sleep(5)
-		url = url = "https://i.instagram.com/api/v1/users/"+str(user_id)+"/info/"
-		user_info = ur.urlopen(url).read()
-		user_name = json.loads(user_info)["user"]["username"]
-		return str(user_name)
-
-	def getMediaCount(self, user_id):
-		time.sleep(5)
-		url = "https://i.instagram.com/api/v1/users/"+str(user_id)+"/info/"
-		user_info = ur.urlopen(url).read()
-		media_count = json.loads(user_info)["user"]["media_count"]
-		return int(media_count)
-
-	def getProfilePic(self, user_id):
-		time.sleep(5)
-		url = "https://i.instagram.com/api/v1/users/"+str(user_id)+"/info/"
-		user_info = ur.urlopen(url).read()
-		profile_pic_url = json.loads(user_info)["user"]["hd_profile_pic_url_info"]["url"]
-		return str(profile_pic_url)
-
-	def sendMessage(self, text):
-		r = ur.urlopen("https://api.telegram.org/bot"+self.api_token+"/sendMessage?", up.urlencode({"chat_id": self.channel, "text": text}).encode("utf-8")).read()
-		print(r)
-		return
-
-	def getNewPost(self, user_id):
-		return instagram.getNewPost(user_id)
-
-	def getStories(self, user_ids):
-		self.sendMessage("Today's Stories:")
-		time.sleep(5)
-		for user_id in user_ids:
-			try:
-				stories = instagram.getStories(user_id)
-			except:
-				stories = ["Error in retrieving Story"]
-			for item in stories:
-				self.sendMessage(str(item))
-			time.sleep(5)
-
-	def createConfig(self, user_ids):
-
-		users_media_counts = []
-		users_profile_pics = []
-
-		for user_id in user_ids:
-			users_media_counts.append(self.getMediaCount(user_id))
-			users_profile_pics.append(self.getProfilePic(user_id))
-
-		config_f = open(self.path_to_config_file, "w")
-		string_to_write = ""
-		for mc in users_media_counts:
-			string_to_write = string_to_write + str(mc) + "\n"
-		p = 0
-		for pb in users_profile_pics:
-			if p < len(users_profile_pics)-1:
-				string_to_write = string_to_write + str(pb) + "\n"
-			else:
-				string_to_write = string_to_write + str(pb)
-		config_f.write(string_to_write)
-		config_f.close()
-
-	def writeConfig(self, users_media_counts, users_profile_pics):
-
-		config_f = open(self.path_to_config_file, "w")
-		string_to_write = ""
-		for mc in users_media_counts:
-			string_to_write = string_to_write + str(mc) + "\n"
-		p = 0
-		for pb in users_profile_pics:
-			if p < len(users_profile_pics)-1:
-				string_to_write = string_to_write + str(pb) + "\n"
-			else:
-				string_to_write = string_to_write + str(pb)
-		config_f.write(string_to_write)
-		config_f.close()
-
-	def instaBot(self, user_ids, rtime = 3480, stime = 21, daily = False, debugging_mode = True):
-
-		if debugging_mode:
-			self.sendMessage("InstaPython process was launched!")
-
-		user_names = []
-		user_names_string = ""
-		for user_id in user_ids:
-			user_names.append(self.getUserName(user_id))
-			user_names_string = user_names_string + self.getUserName(user_id) + "; "
-
-		print("InstaBot initiated with usernames: ", user_names)
-		if debugging_mode:
-			init_message = "InstaBot initiated with usernames: " + user_names_string.rstrip("; ")
-			self.sendMessage(init_message)
-
-		if os.path.isfile(self.path_to_config_file):
-			print("Found existing configuration file!")
-			if debugging_mode:
-				self.sendMessage("Found existing configuration file!")
-		else:
-			self.createConfig(user_ids)
-			print("Created configuration file at: ", self.path_to_config_file)
-			if debugging_mode:
-				self.sendMessage("Created configuration file!")
-
-		config_f = open(self.path_to_config_file, "r")
-		config = config_f.read().splitlines()
-		config_f.close()
-
-		users_media_counts = config[:int(len(config)/2)]
-		users_profile_pics = config[int(len(config)/2):]
-
-		if rtime != 0:
-			rep = 1
-			while rep < rtime:
-				rep = rep + 60
-				time.sleep(60)
-				for i in range(len(user_ids)):
-					rep = rep + 5
-					new_media_count = self.getMediaCount(user_ids[i])
-					if new_media_count > int(users_media_counts[i]):
-						users_media_counts[i] = new_media_count
-						rep = rep + 5
-						new_post = self.getNewPost(user_ids[i])
-						text = "User "+str(self.getUserName(user_ids[i]))+" posted something new: "+new_post[0]+" ;"
-						self.sendMessage(text)
-						for item in new_post[1:]:
-							text = "Raw image can be found here: "+item+" ;"
-							self.sendMessage(text)
-						text = "Link to profile: https://instagram.com/"+str(self.getUserName(user_ids[i]))
-						self.sendMessage(text)
-						self.writeConfig(users_media_counts, users_profile_pics)
-					rep = rep + 5
-					new_profile_pic_url = self.getProfilePic(user_ids[i])
-					if new_profile_pic_url != users_profile_pics[i]:
-						users_profile_pics[i] = new_profile_pic_url
-						text = "User "+str(self.getUserName(user_ids[i]))+" has a new profile pic: "+str(new_profile_pic_url)+" ;"
-						self.sendMessage(text)
-						self.writeConfig(users_media_counts, users_profile_pics)
-				if daily:
-					if datetime.datetime.now().time().hour == stime and datetime.datetime.now().time().minute == 0:
-						self.getStories(user_ids)
-				else:
-					if stime > 12:
-						stime1 = stime-12
-						stime2 = stime
-					else:
-						stime1 = stime
-						stime2 = stime+12
-					if datetime.datetime.now().time().hour == stime1 and datetime.datetime.now().time().minute == 0:
-						self.getStories(user_ids)
-					if datetime.datetime.now().time().hour == stime2 and datetime.datetime.now().time().minute == 0:
-						self.getStories(user_ids)
-
-			self.getStories(user_ids)
-			print("Done! Restart?")
-		else:
-			start_message = "InstaBot started with usernames: " + user_names_string.rstrip("; ")
-			self.sendMessage(start_message)
-			try:
-				while True:
-					time.sleep(60)
-					for i in range(len(user_ids)):
-						new_media_count = self.getMediaCount(user_ids[i])
-						if new_media_count > int(users_media_counts[i]):
-							users_media_counts[i] = new_media_count
-							new_post = self.getNewPost(user_ids[i])
-							text = "User "+str(self.getUserName(user_ids[i]))+" posted something new: "+new_post[0]+" ;"
-							self.sendMessage(text)
-							for item in new_post[1:]:
-								text = "Raw image can be found here: "+item+" ;"
-								self.sendMessage(text)
-							text = "Link to profile: https://instagram.com/"+str(self.getUserName(user_ids[i]))
-							self.sendMessage(text)
-							self.writeConfig(users_media_counts, users_profile_pics)
-						new_profile_pic_url = self.getProfilePic(user_ids[i])
-						if new_profile_pic_url != users_profile_pics[i]:
-							users_profile_pics[i] = new_profile_pic_url
-							text = "User "+str(self.getUserName(user_ids[i]))+" has a new profile pic: "+str(new_profile_pic_url)+" ;"
-							self.sendMessage(text)
-							self.writeConfig(users_media_counts, users_profile_pics)
-					if daily:
-						if datetime.datetime.now().time().hour == stime and datetime.datetime.now().time().minute == 0:
-							self.getStories(user_ids)
-					else:
-						if stime > 12:
-							stime1 = stime-12
-							stime2 = stime
-						else:
-							stime1 = stime
-							stime2 = stime+12
-						if datetime.datetime.now().time().hour == stime1 and datetime.datetime.now().time().minute == 0:
-							self.getStories(user_ids)
-						if datetime.datetime.now().time().hour == stime2 and datetime.datetime.now().time().minute == 0:
-							self.getStories(user_ids)
-			except KeyboardInterrupt:
-				try:
-					self.writeConfig(users_media_counts, users_profile_pics)
-				except:
-					pass
-
-				print("Exiting!")
-		return
-
 class Instagram:
 
 	def __init__(self):
@@ -616,6 +404,218 @@ class InstaLoad:
 				print(status_msg)
 		else:
 			print("File doesn't exist! Please provide a valid filename/path to file!")
+
+class InstaBot:
+
+	instagram = Instagram()
+
+	def __init__(self, path_to_config_file, channel, api_token):
+		self.path_to_config_file = path_to_config_file
+		self.channel = channel
+		self.api_token = api_token
+
+	def getUserName(self, user_id):
+		time.sleep(5)
+		url = url = "https://i.instagram.com/api/v1/users/"+str(user_id)+"/info/"
+		user_info = ur.urlopen(url).read()
+		user_name = json.loads(user_info)["user"]["username"]
+		return str(user_name)
+
+	def getMediaCount(self, user_id):
+		time.sleep(5)
+		url = "https://i.instagram.com/api/v1/users/"+str(user_id)+"/info/"
+		user_info = ur.urlopen(url).read()
+		media_count = json.loads(user_info)["user"]["media_count"]
+		return int(media_count)
+
+	def getProfilePic(self, user_id):
+		time.sleep(5)
+		url = "https://i.instagram.com/api/v1/users/"+str(user_id)+"/info/"
+		user_info = ur.urlopen(url).read()
+		profile_pic_url = json.loads(user_info)["user"]["hd_profile_pic_url_info"]["url"]
+		return str(profile_pic_url)
+
+	def sendMessage(self, text):
+		r = ur.urlopen("https://api.telegram.org/bot"+self.api_token+"/sendMessage?", up.urlencode({"chat_id": self.channel, "text": text}).encode("utf-8")).read()
+		print(r)
+		return
+
+	def getNewPost(self, user_id):
+		return instagram.getNewPost(user_id)
+
+	def getStories(self, user_ids):
+		self.sendMessage("Today's Stories:")
+		time.sleep(5)
+		for user_id in user_ids:
+			try:
+				stories = instagram.getStories(user_id)
+			except:
+				stories = ["Error in retrieving Story"]
+			for item in stories:
+				self.sendMessage(str(item))
+			time.sleep(5)
+
+	def createConfig(self, user_ids):
+
+		users_media_counts = []
+		users_profile_pics = []
+
+		for user_id in user_ids:
+			users_media_counts.append(self.getMediaCount(user_id))
+			users_profile_pics.append(self.getProfilePic(user_id))
+
+		config_f = open(self.path_to_config_file, "w")
+		string_to_write = ""
+		for mc in users_media_counts:
+			string_to_write = string_to_write + str(mc) + "\n"
+		p = 0
+		for pb in users_profile_pics:
+			if p < len(users_profile_pics)-1:
+				string_to_write = string_to_write + str(pb) + "\n"
+			else:
+				string_to_write = string_to_write + str(pb)
+		config_f.write(string_to_write)
+		config_f.close()
+
+	def writeConfig(self, users_media_counts, users_profile_pics):
+
+		config_f = open(self.path_to_config_file, "w")
+		string_to_write = ""
+		for mc in users_media_counts:
+			string_to_write = string_to_write + str(mc) + "\n"
+		p = 0
+		for pb in users_profile_pics:
+			if p < len(users_profile_pics)-1:
+				string_to_write = string_to_write + str(pb) + "\n"
+			else:
+				string_to_write = string_to_write + str(pb)
+		config_f.write(string_to_write)
+		config_f.close()
+
+	def instaBot(self, user_ids, rtime = 3480, stime = 21, daily = False, debugging_mode = True):
+
+		if debugging_mode:
+			self.sendMessage("InstaPython process was launched!")
+
+		user_names = []
+		user_names_string = ""
+		for user_id in user_ids:
+			user_names.append(self.getUserName(user_id))
+			user_names_string = user_names_string + self.getUserName(user_id) + "; "
+
+		print("InstaBot initiated with usernames: ", user_names)
+		if debugging_mode:
+			init_message = "InstaBot initiated with usernames: " + user_names_string.rstrip("; ")
+			self.sendMessage(init_message)
+
+		if os.path.isfile(self.path_to_config_file):
+			print("Found existing configuration file!")
+			if debugging_mode:
+				self.sendMessage("Found existing configuration file!")
+		else:
+			self.createConfig(user_ids)
+			print("Created configuration file at: ", self.path_to_config_file)
+			if debugging_mode:
+				self.sendMessage("Created configuration file!")
+
+		config_f = open(self.path_to_config_file, "r")
+		config = config_f.read().splitlines()
+		config_f.close()
+
+		users_media_counts = config[:int(len(config)/2)]
+		users_profile_pics = config[int(len(config)/2):]
+
+		if rtime != 0:
+			rep = 1
+			while rep < rtime:
+				rep = rep + 60
+				time.sleep(60)
+				for i in range(len(user_ids)):
+					rep = rep + 5
+					new_media_count = self.getMediaCount(user_ids[i])
+					if new_media_count > int(users_media_counts[i]):
+						users_media_counts[i] = new_media_count
+						rep = rep + 5
+						new_post = self.getNewPost(user_ids[i])
+						text = "User "+str(self.getUserName(user_ids[i]))+" posted something new: "+new_post[0]+" ;"
+						self.sendMessage(text)
+						for item in new_post[1:]:
+							text = "Raw image can be found here: "+item+" ;"
+							self.sendMessage(text)
+						text = "Link to profile: https://instagram.com/"+str(self.getUserName(user_ids[i]))
+						self.sendMessage(text)
+						self.writeConfig(users_media_counts, users_profile_pics)
+					rep = rep + 5
+					new_profile_pic_url = self.getProfilePic(user_ids[i])
+					if new_profile_pic_url != users_profile_pics[i]:
+						users_profile_pics[i] = new_profile_pic_url
+						text = "User "+str(self.getUserName(user_ids[i]))+" has a new profile pic: "+str(new_profile_pic_url)+" ;"
+						self.sendMessage(text)
+						self.writeConfig(users_media_counts, users_profile_pics)
+				if daily:
+					if datetime.datetime.now().time().hour == stime and datetime.datetime.now().time().minute == 0:
+						self.getStories(user_ids)
+				else:
+					if stime > 12:
+						stime1 = stime-12
+						stime2 = stime
+					else:
+						stime1 = stime
+						stime2 = stime+12
+					if datetime.datetime.now().time().hour == stime1 and datetime.datetime.now().time().minute == 0:
+						self.getStories(user_ids)
+					if datetime.datetime.now().time().hour == stime2 and datetime.datetime.now().time().minute == 0:
+						self.getStories(user_ids)
+
+			self.getStories(user_ids)
+			print("Done! Restart?")
+		else:
+			start_message = "InstaBot started with usernames: " + user_names_string.rstrip("; ")
+			self.sendMessage(start_message)
+			try:
+				while True:
+					time.sleep(60)
+					for i in range(len(user_ids)):
+						new_media_count = self.getMediaCount(user_ids[i])
+						if new_media_count > int(users_media_counts[i]):
+							users_media_counts[i] = new_media_count
+							new_post = self.getNewPost(user_ids[i])
+							text = "User "+str(self.getUserName(user_ids[i]))+" posted something new: "+new_post[0]+" ;"
+							self.sendMessage(text)
+							for item in new_post[1:]:
+								text = "Raw image can be found here: "+item+" ;"
+								self.sendMessage(text)
+							text = "Link to profile: https://instagram.com/"+str(self.getUserName(user_ids[i]))
+							self.sendMessage(text)
+							self.writeConfig(users_media_counts, users_profile_pics)
+						new_profile_pic_url = self.getProfilePic(user_ids[i])
+						if new_profile_pic_url != users_profile_pics[i]:
+							users_profile_pics[i] = new_profile_pic_url
+							text = "User "+str(self.getUserName(user_ids[i]))+" has a new profile pic: "+str(new_profile_pic_url)+" ;"
+							self.sendMessage(text)
+							self.writeConfig(users_media_counts, users_profile_pics)
+					if daily:
+						if datetime.datetime.now().time().hour == stime and datetime.datetime.now().time().minute == 0:
+							self.getStories(user_ids)
+					else:
+						if stime > 12:
+							stime1 = stime-12
+							stime2 = stime
+						else:
+							stime1 = stime
+							stime2 = stime+12
+						if datetime.datetime.now().time().hour == stime1 and datetime.datetime.now().time().minute == 0:
+							self.getStories(user_ids)
+						if datetime.datetime.now().time().hour == stime2 and datetime.datetime.now().time().minute == 0:
+							self.getStories(user_ids)
+			except KeyboardInterrupt:
+				try:
+					self.writeConfig(users_media_counts, users_profile_pics)
+				except:
+					pass
+
+				print("Exiting!")
+		return
 
 class InstaView:
 
